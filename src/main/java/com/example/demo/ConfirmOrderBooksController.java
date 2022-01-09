dc3aed859a329b9f3ac8f25e9e78d6538d450595
ahmed yasser <ahmedyasser882000@gmail.com>
2022-01-09 16:24:56 +0200

    access username anywhere , handle manager or not , set Table for Book Orders.

--- src/main/java/com/example/demo/ConfirmOrderBooksController.java
+++ src/main/java/com/example/demo/ConfirmOrderBooksController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,12 +14,23 @@ import java.util.ResourceBundle;
 public class ConfirmOrderBooksController implements Initializable {
     @FXML
     TableView<BookOrders> books;
+    @FXML
+    TableColumn<BookOrders,String> firstColumn;
+    @FXML
+    TableColumn<BookOrders,String> secondColumn;
+    @FXML
+    TableColumn<BookOrders,String> thirdColumn;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //TODO query to get all books that should be ordered;
+
+
         //then add them to books
+        firstColumn.setCellValueFactory(new PropertyValueFactory<BookOrders,String>("ISBN"));
+        secondColumn.setCellValueFactory(new PropertyValueFactory<BookOrders,String>("quantityOrdered"));
+        thirdColumn.setCellValueFactory(new PropertyValueFactory<BookOrders,String>("orderDate"));
 
     }
     @FXML
