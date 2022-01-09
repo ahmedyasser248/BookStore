c9e96f54464784fd8655af64e73df50671f8e5d5
ahmed yasser <ahmedyasser882000@gmail.com>
2022-01-09 14:57:33 +0200

    finished signup and added user to database

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -9,5 +9,14 @@ public class User {
     String shippingAddress;
     String phone;
     Boolean manager;
-
+    public User(String firstName,String lastName,String userName,String email,String password,String phone,String shippingAddress,Boolean manager){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.userName=userName;
+        this.password=password;
+        this.phone=phone;
+        this.manager=manager;
+        this.shippingAddress=shippingAddress;
+        this.email=email;
+    }
 }
--- src/main/java/com/example/demo/signUpController.java
+++ src/main/java/com/example/demo/signUpController.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Objects;
+
 public class signUpController {
+    @FXML
+    Pane root;
     @FXML
     TextField username;
     @FXML
@@ -37,11 +47,42 @@ public class signUpController {
             warning.setText("complete data");
             return;
         }
-        Boolean manager = user.getSelectedToggle().toString().equals("manager");
+        Boolean manager = user.getSelectedToggle().toString().contains("manager");
+        System.out.println(manager);
+        User user1 = new User(firstName.getText(), lastname.getText(), username.getText(), emailAddress.getText(), password.getText(), phoneNumber.getText(), shippingAddress.getText(), manager);
+       Boolean result =signup(user1);
+        if(result){
+            Pane singUpPane = null;
+            try {
+                singUpPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainwindow.fxml")));
+                root.getChildren().setAll(singUpPane);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+        }else{
+            warning.setText("check data you entered");
+        }
     }
     Boolean signup(User user){
-        //String query ="INSERT INTO CUSTOMER"
+        Connection connection=utils.getConnection();
+        String query ="INSERT INTO CUSTOMER (Username,Password,Last_Name,First_Name,Email,Phone_Number,Shipping_Address,Manager) values(?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,user.userName);
+            preparedStatement.setString(2,user.password);
+            preparedStatement.setString(3,user.lastName);
+            preparedStatement.setString(4,user.firstName);
+            preparedStatement.setString(5,user.email);
+            preparedStatement.setString(6,user.phone);
+            preparedStatement.setString(7,user.shippingAddress);
+            preparedStatement.setBoolean(8,user.manager);
+            preparedStatement.execute();
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
         return true;
     }
 
