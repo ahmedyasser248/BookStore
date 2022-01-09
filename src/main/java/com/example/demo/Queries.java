b6a51d01911fea707198413de5262a6fdc8d220f
samniss <mohamedelsamni@gmail.com>
2022-01-09 19:28:56 +0200

    added new Queries

--- src/main/java/com/example/demo/Queries.java
+++ src/main/java/com/example/demo/Queries.java
@@ -2,23 +2,21 @@ package com.example.demo;
 
 import java.sql.Connection;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Queries {
-    Book[] searchByCategory(Category categoryName, Connection conn){
+    static ArrayList<Book> searchByCategory(Category categoryName, Connection conn){
         try {
             PreparedStatement getBooksByCategory
-                = conn.prepareStatement("select b.Title,a.Author_Name,b.category"
-                + "from book as b, author as a"
-                + "where b.category ="+categoryName+ "And b.ISBN = a.ISBN;");
+                = conn.prepareStatement("select b.Title,a.Author_Name,b.category "
+                + " from book as b, author as a "
+                + " where b.category ="+categoryName+ "And b.ISBN = a.ISBN;");
             ResultSet  result = getBooksByCategory.executeQuery();
-            int size = result.getFetchSize();
-            Book[] output = new Book[size];
-            int i = 0;
+            ArrayList<Book> output = new ArrayList<>();
             while (result.next()){
-                output[i] = new Book(result.getString("Title"),
+                output.add(new Book(result.getString("Title"),
                     result.getString("Author_Name"),
-                    Category.valueOf(result.getString("Category")));
-                i++;
+                    Category.valueOf(result.getString("Category"))));
             }
             return output;
         } catch (SQLException throwables) {
@@ -27,5 +25,51 @@ public class Queries {
         return null;
     }
 
+    static ArrayList<Book> search(String searchTerm, Connection conn,int searchCriteria){
+        try {
+            String equalityCond = "";
+            if(searchCriteria == 0){
+                equalityCond += " where b.Title = ?";
+            }else if(searchCriteria == 1){
+                equalityCond += " where b.ISBN = ?";
+            }
+            PreparedStatement getBooks
+                = conn.prepareStatement("select b.Title,a.Author_Name,b.category "
+                + " from book as b, author as a "
+                + equalityCond+ " And b.ISBN = a.ISBN;");
+            getBooks.setString(1,searchTerm);
+            ResultSet  result = getBooks.executeQuery();
+
+            ArrayList<Book> output = new ArrayList<>();
+
+            while (result.next()){
+               output.add(new Book(result.getString("Title"),
+                    result.getString("Author_Name"),
+                    Category.valueOf(result.getString("Category"))));
+
+            }
+            return output;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String myDriver = "org.gjt.mm.mysql.Driver";
+        String myUrl = "jdbc:mysql://localhost/bookstore";
+        //Class.forName(myDriver);
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(myUrl, "root", "Soraislife443");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        ArrayList<Book> output = search("12345678",conn,1);
+        for (int i = 0; i < output.size(); i++) {
+            System.out.println(output.get(i).toString());
+        }
+    }
 
 }
